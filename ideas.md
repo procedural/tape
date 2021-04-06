dd2b27e99fc9f299abfea9fb479fb514d05f9e45
Constantine Tarasenkov <iamvfx@gmail.com>
2021-04-06 11:39:01 +0300

    New Tape

--- ideas.md
+++
@@ -1,5 +0,0 @@
-* `?` is the start and the end of a user defined, compile-time replaced named value.
-
-* opcode 3 for relative jump based on the current thread id (from 0 to N) to the address N * 3 + 1, because it's intended to be used with jump instructions that may follow it to diverge the control flow of each thread.
-
-* Parameters can be only one of the following types: IV, IC, IA, OV, OC, OA (I: Input, O: Output, V: Value, C: Count, A: Array). Count must always come right before array parameter.
--- tape.c
+++ tape.c
@@ -1,44 +1,10 @@
-2                    // 2 is a reserved opcode for jump
-main                 // to address 19
-jump                 // if this parameter address value is non-zero; the value of address 30 is 1, so jump
-.helloWorld. 'H'
-'e'
-'l'
-'l'
-'o'
-','
-' '
-'w'
-'o'
-'r'
-'l'
-'d'
-'!'
-'\n'
-.helloWorldCount. 14
-.main. 1
-4                    // 4 is an opcode for, say, "print" procedure that takes two parameters addresses: an address to count and an address to the first character
-helloWorldCount
-helloWorld
-5                    // 5 is an opcode for, say, "decrement" procedure that takes one parameter address: an address to the value to decrement
-loopCount
-2                    // to
-main                 // main
-loopCount            // if loopCount != 0
-0
-.loopCount. 3
-.jump. 1
-// The end comments don't take heap space
-// The program starts at address 1
-// Every line is a 64-bit value
-// Spaces are optional
-// . is the start and the end of a named address declaration
-// ' is the start and the end of a character
-// 0-9 is a part of value
-// a-Z is a part of named address
-// opcode 0 is reserved for program exit
-// opcode 1 is reserved for next opcode
-// opcode 2 is reserved for "jump to the first parameter address if the second parameter address value is non-zero; if zero, continue to the next opcode"
-// ; is the start and the end of a binary number. For example:
-// ; 0000 0000 0000 0000 0000 0000 0000 0000 0000 0000 0000 0000 0000 0000 0000 0000 ;
-// No opcode is allowed to jump except opcode 2 and opcode 3 (see ideas.md)
+
+// The end comments are ignored.
+// Each line is 10 64-bit values.
+// Empty line means 10 64-bit zeros.
+// Partial filling of a line means the rest values of the line are zeros.
+// At program start, "program counter" starts at line 1.
+// The first 64-bit value of a line is an instruction opcode to execute.
+// An instruction can modify all the 64-bit values of the program.
+// An instruction can append new lines to the end of the program.
+// After an instruction is finished executing, it orders the "program counter" to go to line N, from 0 to N, where 0 means "don't go to line N, proceed to the next line".
